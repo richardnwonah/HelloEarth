932556ff26e337278295f40988d173888966e07c
Richard Nwonah <53543378+richardnwonah@users.noreply.github.com>
2023-08-18 14:08:06 +0100

    Project to repo

---
+++ MockBankApp/MockBankApi/AccountDateParameter.cs
@@ -0,0 +1,11 @@
+﻿namespace MockBankApi
+{
+    public class AccountDateParameter
+    {
+        DateTime initialDate = new DateTime(2022, 12, 31);
+        DateTime finalDate = new DateTime(2023, 12, 31);
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+    }
+}
---
+++ MockBankApp/MockBankApi/Services/IAccountService.cs
@@ -0,0 +1,10 @@
+﻿using MockBankApi.Models;
+
+namespace MockBankApi.Services
+{
+    public interface IAccountService
+    {
+        List<Staging> GetAccountService(AccountDateParameter daterange);
+        Staging GetAccountById(int stagingId);
+    }
+}
